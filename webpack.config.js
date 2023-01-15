70460aee1e21ffa805b877b964eccbcf5af67a04
Aste <aste@tuta.io>
2023-01-15 10:31:36 +0100

    Webpack modified

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.(png|svg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            }
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ]
     }
 
